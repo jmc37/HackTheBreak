30448a47f2abe56150c38b33f062aba8a87e89f5
jessicamlee <jessicamlee25@gmail.com>
2023-01-29 22:22:04 -0800

    Outline Hero/Banner section

--- src/sections/HeroBanner.tsx
+++ src/sections/HeroBanner.tsx
@@ -1,10 +1,24 @@
-import React from 'react';
+import React from "react";
+import styles from "../styles/HeroBanner.module.css";
 
 function HeroBanner() {
 
     return(
-        <section>
-            <h2>herobanner section here</h2>
+        <section className={styles.banner}>
+            <h1>Hack the Break 2023</h1>
+            <p className={styles.tagline}>tagline</p>
+            <div className={styles.eventDetails}>
+                <p>March 11-12th</p>
+                <p>"Hybrid Event"</p>
+            </div>
+            <div className={styles.countdown}>
+                {/* countdown here */}
+                <p>Until Registration Closes</p>
+            </div>
+            <div className={styles.bannerBtns}>
+                <button><a href="#" title="Link to Register">Register</a></button>
+                <button><a href="#" title="Link to Learn more">Learn more</a></button>
+            </div>
         </section>
     );
 }
