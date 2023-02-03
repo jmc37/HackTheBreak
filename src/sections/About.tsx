2cded93be299815cfba96d6f79611fc54379386b
jessicamlee <jessicamlee25@gmail.com>
2023-02-02 16:39:48 -0800

    Styled About section

--- src/sections/About.tsx
+++ src/sections/About.tsx
@@ -1,19 +1,21 @@
 import React from 'react';
+import styles from '../styles/About.module.css';
 
 function About() {
 
     return(
-        <section id="about">
-            <div>
-                <h2>About Hack the Break</h2>
+        <section id="about" className={styles.aboutSection}>
+            <h2 className={styles.aboutHeader}>About Hack the Break</h2>
+            <div className={styles.statContainer}>
+                {/* Carousel  */}
+                <div className={styles.statBox}>Stat 1</div>
+                <div className={styles.statBox}>Stat 2</div>
+                <div className={styles.statBox}>Stat 3</div>
+                <div className={styles.statBox}>Stat 4</div>
+            </div>
+            <div className={styles.writtenContainer}>
                 <p>Something to summarize the hackathon and provide info behind the organization (eg, past events, mission, school, who we are). Why bother attending?? Convince the users here.</p>
                 <p>Location, benefits, activities, target audience for participants (eg - new students, anyone curious about hacks), etc.</p>
-            </div>
-            <div>
-                <div>Stat 1</div>
-                <div>Stat 2</div>
-                <div>Stat 3</div>
-                <div>Stat 4</div>
             </div>
         </section>
     );
