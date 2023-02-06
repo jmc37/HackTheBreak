05d3c02ff1e6afc30257ac006d43dcb19501f0fe
jessicamlee <jessicamlee25@gmail.com>
2023-02-05 20:09:41 -0800

    Update Sponsors section styles

--- src/sections/Sponsors.tsx
+++ src/sections/Sponsors.tsx
@@ -1,37 +1,56 @@
 import React from 'react';
 import styles from '../styles/Sponsors.module.css';
-import sponsorLogo1 from '../assets/hackthebreak_logo.png';
+import bcitsaLogo from '../assets/bcitsa_logo.png';
+import ilmLogo from '../assets/ilm_logo.png';
+import sapLogo from '../assets/sap_logo.png';
+import mastercardLogo from '../assets/mastercard_logo.png';
+import teckLogo from '../assets/teck_logo.png';
 
 function Sponsors() {
 
     return(
         <section id="sponsors">
-            <h2>Event Sponsors</h2>
-            <div className={styles.sponsorBox}>
-                <img 
-                    className={styles.sponsorLogo}
-                    src={sponsorLogo1}
-                    alt='Sponsor Logo'
-                />
-                <img 
-                    className={styles.sponsorLogo}
-                    src={sponsorLogo1}
-                    alt='Sponsor Logo'
-                />
-                <img 
-                    className={styles.sponsorLogo}
-                    src={sponsorLogo1}
-                    alt='Sponsor Logo'
-                />
+            <div className="container">
+                <h2>Event Sponsors</h2>
+                <div className={styles.sponsorBox}>
+                    <img 
+                        className={styles.sponsorLogo}
+                        src={bcitsaLogo}
+                        alt='BCIT Student Association Logo'
+                    />
+                    <img 
+                        className={styles.sponsorLogo}
+                        src={ilmLogo}
+                        alt='Industrial Lights and Magic Logo'
+                    />
+                </div>
+                <div className={styles.sponsorBox}>
+                    <img 
+                        className={styles.sponsorLogo}
+                        src={sapLogo}
+                        alt='SAP Software Solutions Logo'
+                    />
+                    <img 
+                        className={styles.sponsorLogo}
+                        src={mastercardLogo}
+                        alt='Mastercard Logo'
+                    />
+                    <img 
+                        className={styles.sponsorLogo}
+                        src={teckLogo}
+                        alt='Teck Resources Logo'
+                    />
+                </div>
+                <button className={styles.sponsorBtn}>
+                    <a 
+                        className={styles.devpostLink}
+                        href="https://hack-the-break-2022.devpost.com/" 
+                        target={"_blank"} 
+                        title="Link to BCIT Hack the Break Devpost">
+                            Find full details about us on <span className={styles.devpostHighlight}>Devpost</span>!
+                        </a>
+                </button>
             </div>
-            <button className={styles.sponsorBtn}>
-                <a 
-                    href="https://hack-the-break-2022.devpost.com/" 
-                    target={"_blank"} 
-                    title="Link to BCIT Hack the Break Devpost">
-                        Find full details about us on Devpost!
-                    </a>
-            </button>
         </section>
     );
 }
