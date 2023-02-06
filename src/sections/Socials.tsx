cae54695701a786b051a0f864edcc07c43f1a6c9
jessicamlee <jessicamlee25@gmail.com>
2023-02-05 21:19:49 -0800

    Update Socials section styles, Rename asset

--- src/sections/Socials.tsx
+++ src/sections/Socials.tsx
@@ -1,16 +1,45 @@
 import React from 'react';
+import styles from '../styles/Socials.module.css';
+import computingClubLogo from '../assets/computingclub_logo.png';
+import bcitWicLogo from '../assets/bcitwic_logo.png';
+import coding4all from '../assets/coding4all_logo.png';
 
 function Socials() {
 
     return(
-        <section>
-            <h2>Hack the Break 2023 <br/> brought to you by:</h2>
-            <ul>
-                <li><a>social icon here</a></li>
-                <li><a>social icon here</a></li>
-                <li><a>social icon here</a></li>
-                <li><a>social icon here</a></li>
-            </ul>
+        <section className={styles.socialsSection}>
+            <div className="container">
+                <h2 className={styles.socialsHeading}>Hack the Break 2023 <br/> brought to you by:</h2>
+                <ul className={styles.socialsContainer}>
+                    <li>
+                        <a
+                            href="#"
+                            target="_blank"
+                            title="Link to Computing Club"
+                        >
+                            <img src={computingClubLogo} />
+                        </a>
+                    </li>
+                    <li>
+                        <a
+                            href="#"
+                            target="_blank"
+                            title="Link to BCIT Women in Computing"
+                        >
+                            <img src={bcitWicLogo} />
+                        </a>
+                    </li>
+                    <li>
+                        <a
+                            href="#"
+                            target="_blank"
+                            title="Link to Coding 4 All"
+                        >
+                            <img src={coding4all} />
+                        </a>
+                    </li>
+                </ul>
+            </div>
         </section>
     );
 }
