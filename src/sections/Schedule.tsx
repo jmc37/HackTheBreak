efc408a49f04029033d0ca2a919718927bbc8b0d
jessicamlee <jessicamlee25@gmail.com>
2023-02-05 19:44:11 -0800

    Update Schedule section styles

--- src/sections/Schedule.tsx
+++ src/sections/Schedule.tsx
@@ -1,16 +1,40 @@
 import React from 'react';
+import styles from '../styles/Schedule.module.css';
 
 function Schedule() {
 
     return(
-        <section id="schedule">
-            <h2>Hackathon Schedule</h2>
-            <div>
-                <div>Event 1</div>
-                <div>Event 2</div>
-                <div>Event 3</div>
-                <div>Event 4</div>
-                <div>Event 5</div>
+        <section id="schedule" className={styles.scheduleSection}>
+            <div className="container">
+                <h2>Hackathon Schedule</h2>
+                <div className={styles.scheduleContainer}>
+                    <p className={styles.hackDay}>Day 1</p>
+                    <div className={styles.activityContainer}>
+                        <div className={styles.activityBox}>
+                            <p className={styles.activityTime}>00:00</p>
+                            <p className={styles.activityName}>Activity Name</p>
+                        </div>
+                        <div className={styles.activityBox}>
+                            <p className={styles.activityTime}>00:00</p>
+                            <p className={styles.activityName}>Activity Name</p>
+                        </div>
+                    </div>
+                    <p className={styles.hackDay}>Day 2</p>
+                    <div className={styles.activityContainer}>
+                        <div className={styles.activityBox}>
+                            <p className={styles.activityTime}>00:00</p>
+                            <p className={styles.activityName}>Activity Name</p>
+                        </div>
+                        <div className={styles.activityBox}>
+                            <p className={styles.activityTime}>00:00</p>
+                            <p className={styles.activityName}>Activity Name</p>
+                        </div>
+                        <div className={styles.activityBox}>
+                            <p className={styles.activityTime}>00:00</p>
+                            <p className={styles.activityName}>Activity Name</p>
+                        </div>
+                    </div>
+                </div>
             </div>
         </section>
     );
