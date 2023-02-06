e6a98d71443c26ce4c67c28e859f66bdbb19f66e
jessicamlee <jessicamlee25@gmail.com>
2023-02-05 23:34:05 -0800

    Add and update Contact Form section styles

--- src/sections/Contact.tsx
+++ src/sections/Contact.tsx
@@ -1,12 +1,49 @@
 import React from 'react';
+import styles from '../styles/Contact.module.css';
+import TextField from '@mui/material/TextField';
 
 function Contact() {
 
     return(
         <section id="contact">
-            <h2>Send Us A Message</h2>
-            <p>Question still unanswered? Interested in sponsoring Hack the Break 2023? Reach out to us below:</p>
-            <div>contact form here</div>
+            <div className="container">
+                <h2>Send Us A Message</h2>
+                <p>Question still unanswered? Interested in sponsoring Hack the Break 2023? <span className={styles.faqHighlight}>Reach out to us below:</span></p>
+                <form className={styles.contactForm}>
+                    <div className={styles.contactCols}>
+                        <TextField 
+                            required
+                            label="Your Name"
+                            fullWidth
+                        />
+                        <TextField 
+                            required
+                            label="Your Email Address"
+                            fullWidth
+                        />
+                        <TextField 
+                            required
+                            label="Subject Line"
+                            fullWidth
+                        />
+                    </div>
+                    <div className={styles.contactCols}>
+                        <TextField 
+                            required
+                            label="Message here"
+                            fullWidth
+                            multiline
+                            rows={3}
+                        />
+                        <button 
+                            type="submit"
+                            className={styles.sendBtn}
+                        >
+                            Send Message
+                        </button>
+                    </div>
+                </form>
+            </div>
         </section>
     );
 }
