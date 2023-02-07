47748ea2b011dc8486d8026521237ebb78628459
jessicamlee <jessicamlee25@gmail.com>
2023-02-06 20:37:16 -0800

    Style Sign up/Registration Form section

--- src/sections/SignupForm/FormWrapper.tsx
+++ src/sections/SignupForm/FormWrapper.tsx
@@ -1,4 +1,5 @@
 import { ReactNode } from "react";
+import styles from "../../styles/SignupForm.module.css";
 
 type FormWrapperProps = {
   title: string;
@@ -8,17 +9,8 @@ type FormWrapperProps = {
 export function FormWrapper({ title, children }: FormWrapperProps) {
   return (
     <>
-      <h2 style={{ textAlign: "center", margin: 0, marginBottom: "2rem" }}>
-        {title}
-      </h2>
-      <div
-        style={{
-          display: "grid",
-          gap: "1rem .5rem",
-          justifyContent: "flex-start",
-          gridTemplateColumns: "auto minmax(auto, 400px)",
-        }}
-      >
+      <h3>{title}</h3>
+      <div className={styles.formBox}>
         {children}
       </div>
     </>
--- src/sections/SignupForm/SignupForm.tsx
+++ src/sections/SignupForm/SignupForm.tsx
@@ -1,4 +1,4 @@
-import styles from '../styles/SignupForm.module.css';
+import styles from '../../styles/SignupForm.module.css';
 import { FormEvent, useState } from "react";
 import { AccountForm } from "./AccountForm";
 import { AddressForm } from "./AddressForm";
@@ -120,41 +120,35 @@ function SignupForm() {
   }
 
     return(
-        <div
-            id="signupform"
-            style={{
-              position: "relative",
-              background: "white",
-              border: "1px solid black",
-              padding: "2rem",
-              margin: "1rem",
-              borderRadius: ".5rem",
-              fontFamily: "Arial",
-              maxWidth: "max-content",
-            }}
-          >
-            <form onSubmit={onSubmit}>
-              <div style={{ position: "absolute", top: ".5rem", right: ".5rem" }}>
-                {currentStepIndex + 1} / {steps.length}
+        <section id="signupform" className={styles.signupSection}>
+          <div className="container">
+            <div className={styles.signupDesc}>
+              <h2>Register as a hacker!</h2>
+              <p>Join Hack the Break 2023 by registering <br className={styles.lineBreak} /> as a hacker in the form below:</p>
+            </div>
+            <form 
+              className={styles.signupContainer}
+              onSubmit={onSubmit}
+            >
+              <div className={styles.signupPageCounter}>
+                {currentStepIndex + 1}/{steps.length}
               </div>
               {step}
-              <div
-                style={{
-                  marginTop: "1rem",
-                  display: "flex",
-                  gap: ".5rem",
-                  justifyContent: "flex-end",
-                }}
-              >
+              <div className={styles.signupBtnContainer}>
                 {!isFirstStep && (
-                  <button type="button" onClick={back}>
+                  <button 
+                    className={styles.nextBtn}
+                    type="button" 
+                    onClick={back}
+                  >
                     Back
                   </button>
                 )}
-                <button type="submit">{isLastStep ? "Submit" : "Next"}</button>
+                <button type="submit" className={styles.submitBtn}>{isLastStep ? "Submit" : "Next"}</button>
               </div>
             </form>
-        </div>
+          </div>
+        </section>
     );
 }
 
