2376845aa822b7ce20dcc0d412f3453a3d9d91ce
jessicamlee <jessicamlee25@gmail.com>
2023-02-19 17:09:53 -0800

    Update hamburger icon

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,6 +1,7 @@
 import React, { useState, useEffect } from 'react';
 import styles from './Header.module.css';
 import { links } from './anchorTags.json';
+import MenuSharpIcon from '@mui/icons-material/MenuSharp';
 
 type Link = {
     label: string;
@@ -64,11 +65,7 @@ const Header: React.FC<{}> = () => {
                         setIsNavExpanded(!isNavExpanded);
                     }}
                 > 
-                    <svg width="35" height="27" viewBox="0 0 35 27" fill="none" xmlns="http://www.w3.org/2000/svg">
-                        <rect width="35" height="3" rx="1.5" fill="#2B2B2B"/>
-                        <rect y="12" width="35" height="3" rx="1.5" fill="#2B2B2B"/>
-                        <rect y="24" width="35" height="3" rx="1.5" fill="#2B2B2B"/>
-                    </svg>
+                    <MenuSharpIcon fontSize="large" aria-label="hamburger menu"></MenuSharpIcon>
                 </button>
                 <nav className={
                     isNavExpanded ? styles.gnExpanded : styles.gn
