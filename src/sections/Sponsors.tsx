67da2e792ae16c02b653010d0c23fbbc5809aa1f
Joseph Chan <josephchan36@gmail.com>
2023-02-25 19:27:59 -0800

    modified sponsor links

--- src/sections/Sponsors.tsx
+++ src/sections/Sponsors.tsx
@@ -11,30 +11,38 @@ function Sponsors() {
       <div className="container">
         <h2>Event Sponsors</h2>
         <div className={styles.sponsorBox}>
-          <img
-            className={styles.sponsorLogoBCITSA}
-            src={bcitsaLogo}
-            alt="BCIT Student Association Logo"
-          />
-          <img
-            className={styles.sponsorLogo}
-            src={mastercardLogo}
-            alt="Mastercard Logo"
-          />
-          <img
-            className={styles.sponsorLogo}
-            src={sapLogo}
-            alt="SAP Software Solutions Logo"
-          />
+          <a href="https://www.bcitsa.ca/">
+            <img
+              className={styles.sponsorLogoBCITSA}
+              src={bcitsaLogo}
+              alt="BCIT Student Association Logo"
+            />
+          </a>
+          <a href="https://www.mastercard.ca/">
+            <img
+              className={styles.sponsorLogo}
+              src={mastercardLogo}
+              alt="Mastercard Logo"
+            />
+          </a>
+          <a href="https://www.sap.com/index.html">
+            <img
+              className={styles.sponsorLogo}
+              src={sapLogo}
+              alt="SAP Software Solutions Logo"
+            />
+          </a>
         </div>
         <div className={styles.sponsorBox}>
-          <img className={styles.sponsorLogo} src={eaLogo} alt="EA Logo" />
+          <a href="https://www.ea.com/">
+            <img className={styles.sponsorLogo} src={eaLogo} alt="EA Logo" />
+          </a>
         </div>
         <button className={styles.sponsorBtn}>
           <a
             className={styles.devpostLink}
             href="https://hack-the-break-2023.devpost.com/?preview_token=xv4Hin%2Bqb5evDNjBNiMqTIR7BvKqh7xZt6rLEke49Co%3D"
-            target={"_blank"}
+            target="_blank"
             title="Link to BCIT Hack the Break Devpost"
           >
             Find full details about us on{" "}
